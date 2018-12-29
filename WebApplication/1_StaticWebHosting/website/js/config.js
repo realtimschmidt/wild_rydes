2d9d2540e73f2a02dd77638027f24f8d2e9fb4b6
realtimschmidt <schmidt.tim.j@gmail.com>
2018-12-29 16:28:58 -0500

    rearrange directories

---
+++ WebApplication/1_StaticWebHosting/website/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_d4q08iyEp',
+        userPoolClientId: '10k4afjhile9dli3vkl4f3svj4',
+        region: 'us-east-1'
+    },
+    api: {
+        invokeUrl: 'https://6co96q8j1e.execute-api.us-east-1.amazonaws.com/prod'
+    }
+};
